c984b89bb5e7da590ea6911d490eae8a8b9ac2e4
projects00 <projectdealpublish@gmail.com>
2018-01-11 22:53:10 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { ViewCardComponent } from './view-card/view-card.component';
+import { CreateCardComponent } from './create-card/create-card.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ViewCardComponent,
+    CreateCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'View-Card',
+        component: ViewCardComponent
+      },
+      {
+        path: 'Create-Card',
+        component: CreateCardComponent
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
