aefc0d53469fbe26243a7167ec3ff4f601e31e8a
bhuvanasrini <projectdealpublish@gmail.com>
2018-01-19 16:14:22 +0530

    date updated

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import {DataService} from '../data.service';
+import { DataService } from '../data.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 declare var $: any;
 import 'rxjs/Rx';
 import { Router } from '@angular/router';
@@ -12,46 +13,53 @@ import { Router } from '@angular/router';
 })
 export class CardComponent implements OnInit {
 
-title:String;
-name:String;
-message:String;
- test:any;
-rForm: FormGroup;
-fb1: FormBuilder;
-   constructor(private dataservice: DataService, fb: FormBuilder, private router: Router) {
+  title: String;
+  name: String;
+  message: String;
+  today:String;
+  test: any;
+  dt: Date = new Date();
+  rForm: FormGroup;
+  fb1: FormBuilder;
+  dtformat:String=`dd/MM/yyyy HH:mm`;
+  constructor(private dataservice: DataService, fb: FormBuilder, private router: Router) {
     this.fb1 = fb;
-  this.initilizeFrom();
+    this.initilizeFrom();
 
   }
 
-    initilizeFrom() {
+  initilizeFrom() {
     this.rForm = this.fb1.group({
       'viewTitle': [null, Validators.required],
       'viewName': [null, Validators.required],
       'viewMessage': [null, Validators.required]
-      
+
     });
     this.rForm.controls['viewTitle'].setValue(this.dataservice.title);
     this.rForm.controls['viewName'].setValue(this.dataservice.name);
     this.rForm.controls['viewMessage'].setValue(this.dataservice.message);
 
-       
+
   }
   ngOnInit() {
-    this.title=this.dataservice.title;
-    this.name=this.dataservice.name;
-    this.message=this.dataservice.message;
+    this.title = this.dataservice.title;
+    this.name = this.dataservice.name;
+    this.message = this.dataservice.message;
+    
   }
-  
-  clicked(){
-   this.dataservice.title=this.rForm.value.viewTitle;
-      this.dataservice.name=this.rForm.value.viewName;
-       this.dataservice.message=this.rForm.value.viewMessage;
-     $("#myModal").modal("toggle");
-      this.router.navigateByUrl('Card/123');
-       this.title=this.dataservice.title;
-    this.name=this.dataservice.name;
-    this.message=this.dataservice.message;
+
+  clicked() {
+    this.dataservice.title = this.rForm.value.viewTitle;
+    this.dataservice.name = this.rForm.value.viewName;
+    this.dataservice.message = this.rForm.value.viewMessage;
+    $("#myModal").modal("toggle");
+    this.router.navigateByUrl('Card/123');
+    this.title = this.dataservice.title;
+    this.name = this.dataservice.name;
+    this.message = this.dataservice.message;
+    const d: Date = new Date();
+       this.dt =d;
+    
   }
 
 }
