f009c4ffed62324f9f7c5b38e2dc136c35ede63e
bhuvanasrini <projectdealpublish@gmail.com>
2018-01-20 18:17:53 +0530

    date and dropdown selection based image is added

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -17,14 +17,21 @@ export class CardComponent implements OnInit {
   name: String;
   message: String;
   today:String;
-  test: any;
+  theme: String;
+  chosenOption:String;
   dt: Date = new Date();
   rForm: FormGroup;
   fb1: FormBuilder;
+  options = [
+    {name: "option1", value:1},
+    {name: "option2", value:2},
+     {name: "option2", value:2}
+  ]
   dtformat:String=`dd/MM/yyyy HH:mm`;
   constructor(private dataservice: DataService, fb: FormBuilder, private router: Router) {
     this.fb1 = fb;
     this.initilizeFrom();
+    this.onChange();
 
   }
 
@@ -32,31 +39,55 @@ export class CardComponent implements OnInit {
     this.rForm = this.fb1.group({
       'viewTitle': [null, Validators.required],
       'viewName': [null, Validators.required],
-      'viewMessage': [null, Validators.required]
+      'viewMessage': [null, Validators.required],
+      'viewTheme':["./assets/img/img-01.png"]
 
     });
     this.rForm.controls['viewTitle'].setValue(this.dataservice.title);
     this.rForm.controls['viewName'].setValue(this.dataservice.name);
     this.rForm.controls['viewMessage'].setValue(this.dataservice.message);
-
+    this.rForm.controls['viewTheme'].setValue(this.dataservice.theme);
 
   }
+ 
+
   ngOnInit() {
-    this.title = this.dataservice.title;
+     this.title = this.dataservice.title;
     this.name = this.dataservice.name;
     this.message = this.dataservice.message;
+     this.theme = this.dataservice.theme;
+  }
+
+  onChange(){
+    debugger;
+switch (this.rForm.value.viewTheme) {
+    case "1":
+      this.theme="./assets/img/img-01.png";
+      break;
+    case "2":
+      this.theme="./assets/img/img-02.png";
+      break;
+    case "3":
+      this.theme="./assets/img/img-03.png";
+      break;
+
     
   }
 
+  }
   clicked() {
+    debugger;
+   // this.theme=this.rForm.value.viewTheme;
     this.dataservice.title = this.rForm.value.viewTitle;
     this.dataservice.name = this.rForm.value.viewName;
     this.dataservice.message = this.rForm.value.viewMessage;
+    this.dataservice.theme = this.rForm.value.viewTheme;
     $("#myModal").modal("toggle");
     this.router.navigateByUrl('Card/123');
     this.title = this.dataservice.title;
     this.name = this.dataservice.name;
     this.message = this.dataservice.message;
+   //this.theme=this.dataservice.theme;
     const d: Date = new Date();
        this.dt =d;
     
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -5,6 +5,7 @@ export class DataService {
 title:String="title test data";
 name:String="sample name";
 message:String="sample Message";
+theme:String="./assets/img/img-01.png"
  dt: Date = new Date();
   constructor() { }
 
